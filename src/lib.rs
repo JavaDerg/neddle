1eb3c9749c2100333ab1968f51fb6251ac265f12
PostRex <post-rex@pm.me>
2021-11-24 14:25:27 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,129 @@
+#[cfg(test)]
+mod tests;
+
+use fxhash::FxBuildHasher;
+use std::any::{Any, TypeId};
+
+pub trait Injectable<R, Param> {
+    fn ctx_call(&self, ctx: &Context) -> R;
+    fn call_direct(&self, p: Param) -> R;
+}
+
+pub struct Context {
+    map: dashmap::DashMap<TypeId, Box<dyn Any>, FxBuildHasher>,
+}
+
+pub trait FromCtx {
+    fn from(ctx: &Context) -> Self;
+}
+
+impl Context {
+    pub fn new() -> Self {
+        Self {
+            map: Default::default(),
+        }
+    }
+}
+
+impl<F, R> Injectable<R, ()> for F
+where
+    F: Fn() -> R,
+{
+    fn ctx_call(&self, _ctx: &Context) -> R {
+        self()
+    }
+
+    fn call_direct(&self, _: ()) -> R {
+        self()
+    }
+}
+
+impl<T: FromCtx> FromCtx for Option<&T> {
+    fn from(ctx: &Context) -> Self {
+        // ctx.map.get(&TypeId::of::<T>())?.value().downcast_ref()
+        todo!()
+    }
+}
+
+macro_rules! gen_impls
+{
+    [] => {};
+
+    [($($name:ident),+) <- ($($type:ident),+) $(,)?] => {
+        #[allow(unused_parens)]
+        impl<F, R, $($type),+> Injectable<R, ($($type),+)> for F
+        where
+            F: Fn($($type),+) -> R,
+            $($type: FromCtx),+
+        {
+            fn ctx_call(&self, ctx: &Context) -> R {
+                $(
+                    let $name = FromCtx::from(ctx);
+                )+
+                self($($name),+)
+            }
+
+            fn call_direct(&self, ($($name),+): ($($type),+)) -> R {
+                self($($name),+)
+            }
+        }
+    };
+
+    [
+        ($($name_:ident),+) <- ($($type_:ident),+),
+        $(($($name:ident),+) <- ($($type:ident),+)),+
+        $(,)?
+    ] => {
+        gen_impls!(($($name_),+) <- ($($type_),+));
+        gen_impls!($(($($name),+) <- ($($type),+)),+);
+    };
+
+    [explode:
+        ($($name_:ident),+) <- ($($type_:ident),+),
+    ] => {
+
+    };
+
+    [snip:
+        ($name_:ident $(, $($name:ident),*)?) <- ($type_:ident $(, $($type:ident),*)?),
+    ] => {
+        gen_impls![
+            explode:
+        ]
+    };
+}
+
+gen_impls![
+    (arg0) <- (Arg0),
+    (arg0, arg1) <- (Arg0, Arg1),
+    (arg0, arg1, arg2) <- (Arg0, Arg1, Arg2),
+    (arg0, arg1, arg2, arg3) <- (Arg0, Arg1, Arg2, Arg3),
+    (arg0, arg1, arg2, arg3, arg4) <- (Arg0, Arg1, Arg2, Arg3, Arg4),
+    (arg0, arg1, arg2, arg3, arg4, arg5) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19, Arg20),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19, Arg20, Arg21),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21, arg22) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19, Arg20, Arg21, Arg22),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21, arg22, arg23) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19, Arg20, Arg21, Arg22, Arg23),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21, arg22, arg23, arg24) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19, Arg20, Arg21, Arg22, Arg23, Arg24),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21, arg22, arg23, arg24, arg25) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19, Arg20, Arg21, Arg22, Arg23, Arg24, Arg25),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21, arg22, arg23, arg24, arg25, arg26) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19, Arg20, Arg21, Arg22, Arg23, Arg24, Arg25, Arg26),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21, arg22, arg23, arg24, arg25, arg26, arg27) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19, Arg20, Arg21, Arg22, Arg23, Arg24, Arg25, Arg26, Arg27),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21, arg22, arg23, arg24, arg25, arg26, arg27, arg28) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19, Arg20, Arg21, Arg22, Arg23, Arg24, Arg25, Arg26, Arg27, Arg28),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21, arg22, arg23, arg24, arg25, arg26, arg27, arg28, arg29) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19, Arg20, Arg21, Arg22, Arg23, Arg24, Arg25, Arg26, Arg27, Arg28, Arg29),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21, arg22, arg23, arg24, arg25, arg26, arg27, arg28, arg29, arg30) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19, Arg20, Arg21, Arg22, Arg23, Arg24, Arg25, Arg26, Arg27, Arg28, Arg29, Arg30),
+    (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21, arg22, arg23, arg24, arg25, arg26, arg27, arg28, arg29, arg30, arg31) <- (Arg0, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16, Arg17, Arg18, Arg19, Arg20, Arg21, Arg22, Arg23, Arg24, Arg25, Arg26, Arg27, Arg28, Arg29, Arg30, Arg31),
+];
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use neddle::{Context, FromCtx, Injectable};
+
+fn main() {
+    let ctx = Context::new();
+    infer.ctx_call(&ctx);
+    infer2.ctx_call(&ctx);
+}
+
+struct Num(u32);
+struct Str(&'static str);
+#[derive(Debug)]
+struct Test(u32, f32);
+
+fn infer(num: Num, str: Str, test: Test) {
+    println!("{}: {}", num.0, str.0);
+    println!("{:?}", test);
+}
+
+fn infer2(test: Test) {
+    println!("{}, {}", test.0, test.1);
+}
+
+impl FromCtx for Test {
+    fn from(_ctx: &Context) -> Self {
+        Test(123, 321.31)
+    }
+}
+
+impl FromCtx for Num {
+    fn from(_ctx: &Context) -> Self {
+        Num(0)
+    }
+}
+
+impl FromCtx for Str {
+    fn from(_ctx: &Context) -> Self {
+        Str("Hello world")
+    }
+}
